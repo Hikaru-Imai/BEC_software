f24ef6a58141dc9b168ec6589e2fc6bd8b7965b1
Hikaru Imai <hikaru@HikarunoMac-mini.local>
2021-12-02 00:43:16 +0900

    bulid

---
+++ bulid/CMakeFiles/RUN.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../RUN"
+  "../RUN.pdb"
+  "CMakeFiles/RUN.dir/Detection.C.o"
+  "CMakeFiles/RUN.dir/Detection.C.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RUN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
